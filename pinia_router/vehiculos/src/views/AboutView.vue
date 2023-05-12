7119582916aefce1fd5ec9f4075e52b323c12f2e
Felix Ivan Lamas <felix.lamas@hotmail.com>
2023-05-11 22:45:41 -0300

    agregando archivos

---
+++ pinia_router/vehiculos/src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+const { count } = storeToRefs(store);
+const { lista } = store;
+const auto = {
+  marca: "",
+  modelo: "",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Otra página</h1>
+  </div>
+  <h1>Esta página no era accesible sin login</h1>  
+  <h2>Accede a la misma información a través del store</h2>
+  <table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Marca</th>
+        <th scope="col">Modelo</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(auto, index) in lista" :key="index">
+        <th scope="row">{{ auto.id }}</th>
+        <td>{{ auto.marca }}</td>
+        <td>{{ auto.modelo }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style>
+</style>
+
+
---
+++ pinia_router/vehiculos/src/views/Users.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import TablaUsuarios from '../components/TablaUsuarios.vue';
+</script>
+
+<template>
+  <h1>Listado de Usuarios</h1>
+  <RouterLink to="/"><button class="btn btn-warning m-2">Home</button></RouterLink>
+  <TablaUsuarios></TablaUsuarios>
+</template>
+
+<style>
+</style>
